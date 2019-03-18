ba2f02f7e9f92d8517778639133364b4ac454e5c
RDoney <theryandoney@gmail.com>
2019-03-18 17:54:04 -0500

    graphql query for gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,7 +3,17 @@ import React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
 
-const Gallery = () => <Layout />;
+const Gallery = ({ data }) => (
+	<Layout>
+		<section className="section">
+			<div className="container">
+				{data.allFile.edges.map(({ node }, index) => (
+					<img key={index} src={node.childImageSharp.resize.src} />
+				))}
+			</div>
+		</section>
+	</Layout>
+);
 
 export const query = graphql`
 	query {
@@ -13,6 +23,9 @@ export const query = graphql`
 					relativePath
 					childImageSharp {
 						id
+						resize {
+							src
+						}
 					}
 				}
 			}
